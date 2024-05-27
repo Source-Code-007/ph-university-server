1785a2164723a8dea49e28ae066098c25010de68
Source-Code-007 <utsho926@gmail.com>
2024-05-27 15:56:24 +0600

    error handler added

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import { studentRouter } from './app/module/student/student.route'
 import { userRoute } from './app/module/user/user.route'
+import { globalErrHandler, notFoundErrHandler } from './app/middleware/errHandler'
 
 const app = express()
 
@@ -13,5 +14,11 @@ app.use(express.json())
 // Router
 app.use('/api/v1/students', studentRouter)
 app.use('/api/v1/users', userRoute)
+
+
+
+// error handler
+app.use(notFoundErrHandler)
+app.use(globalErrHandler)
 
 export default app
---
+++ src/app/middleware/errHandler.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const notFoundErrHandler = (req:Request, res:Response, next:NextFunction)=> {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(StatusCodes.NOT_FOUND).send({success:false, message: error?.message, error: error})
+}
+const globalErrHandler = (err:any, req:Request, res:Response, next:NextFunction)=> {
+    console.log(res.status, 'res.status');
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({success:false, message: err?.message || 'Internal server error', error: err})
+}
+
+export {notFoundErrHandler, globalErrHandler}
